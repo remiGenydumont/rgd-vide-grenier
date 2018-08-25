24efcf8e56ddae89ec324139280c27917684da0c
rgeny <remi.genydumont@gmail.com>
2018-08-25 19:21:29 +0200

    adding first controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky", name="lucky")
+     */
+    public function index()
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+        'number' => $number,
+    ]);
+    }
+}
